a4252fb2c01e66be0957d72be65418070e46bcf0
jose.cambronero <jose.cambronero@cloudera.com>
2015-07-20 17:31:22 -0700

    added KS example

--- src/main/scala/com/cloudera/examples/AndersonDarlingMTA.scala
+++ src/main/scala/com/cloudera/examples/AndersonDarlingMTA.scala
@@ -13,7 +13,6 @@
  * License.
  */
 
-
 package com.cloudera.examples
 
 import org.apache.commons.math3.distribution.ExponentialDistribution
@@ -22,46 +21,26 @@ import scala.util.Try
 
 import org.joda.time.DateTime
 
+import org.apache.spark.rdd.RDD
+
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.stat.Statistics
 
 object AndersonDarlingMTA {
-  def main(a: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
     val sc = new SparkContext("local", "test")
     // TODO: replace with hdfs path in cluster
     val path = "/Users/josecambronero/Projects/gof/data/mta_may_2011"
     val mtaRaw = sc.textFile(path).sample(false, 0.3, 3L) // just a sample for now, since local
     val parsed = mtaRaw.map(x => Try(parseTrainEvent(x))).filter(_.isSuccess).map(_.get)
     // my train: 6, downtown, arrival times :)
-    val train6Down= parsed.filter { x =>
+    val train6Down = parsed.filter (x =>
       x.train == 6 && x.dir == 1 && x.eventType == 1
-    }.sortBy(_.timestamp) // go ahead and sort now
+    ).sortBy(x => x.timestamp.getMillis ) // go ahead and sort now
 
 
-    val stops = train6Down.map(_.stop).distinct
-    val results = stops.map { stop =>
-      val stopData = train6Down.filter(_.stop == stop).map(_.timestamp)
-      // to calculate time span between arrivals we need to process elements sequentially
-      // (to some extent). To take advantage of spark, we want to parallelize though
-      // so the compromise is: processes partitions in parallel, and operate sequentially
-      // within each partition. This means that we need to drop 1 span per partition (the first
-      // one), and we drop any observations that are alone in a partition.
-      val waitingTimes = stopData.mapPartitions { part =>
-        val local = part.toArray
-        local.zip(local.drop(1)).map { case (pt, t) =>
-          (t.getMillis - pt.getMillis).toDouble / 6e4
-        }.iterator
-      }.filter(_ > 0)
-      // We estimate the MLE for exponential (i.e. the mean of the data)
-      val expDist = new ExponentialDistribution(waitingTimes.mean())
-      // Just using KS as a place holder, this should be AD, since it is robust
-      // against estimating parameters from the data that we're testing
-      val result = Statistics.kolmogorovSmirnovTest(
-        waitingTimes,
-        (span: Double) => expDist.cumulativeProbability(span)
-        )
-      (stop, result)
-    }
+    val stops = train6Down.map(_.stop).distinct.take(3)
+    val results = stops.map(stop => Try(testExpDistPerStop(train6Down, stop)))
   }
 
   case class TrainEvent(
@@ -86,5 +65,29 @@ object AndersonDarlingMTA {
     val stop = splitLine(6).toInt
     val track = splitLine(7)
     TrainEvent(timestamp, train, dir, eventType, route, stop, track)
+  }
+
+  def testExpDistPerStop(data: RDD[TrainEvent], stop: Int) = {
+    val stopData = data.filter(_.stop == stop).map(_.timestamp)
+    // to calculate time span between arrivals we need to process elements sequentially
+    // (to some extent). To take advantage of spark, we want to parallelize though
+    // so the compromise is: processes partitions in parallel, and operate sequentially
+    // within each partition. This means that we need to drop 1 span per partition (the first
+    // one), and we drop any observations that are alone in a partition.
+    val waitingTimes = stopData.mapPartitions { part =>
+      val local = part.toArray
+      local.zip(local.drop(1)).map { case (pt, t) =>
+        (t.getMillis - pt.getMillis).toDouble / 6e4
+      }.iterator
+    }.filter(_ > 0)
+    // We estimate the MLE for exponential (i.e. the mean of the data)
+    val expDist = new ExponentialDistribution(waitingTimes.mean())
+    // Just using KS as a place holder, this should be AD, since it is robust
+    // against estimating parameters from the data that we're testing
+    val result = Statistics.kolmogorovSmirnovTest(
+      waitingTimes,
+      (span: Double) => expDist.cumulativeProbability(span)
+    )
+    (stop, result)
   }
 }
---
+++ src/main/scala/com/cloudera/examples/KolmogorovSmirnovOLS.scala
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2015, Cloudera, Inc. All Rights Reserved.
+ *
+ * Cloudera, Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+
+package com.cloudera.examples
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+import org.apache.spark.mllib.stat.Statistics
+import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD}
+import org.apache.spark.mllib.linalg.Vectors
+
+import org.apache.spark.mllib.random.RandomRDDs.normalRDD
+import org.apache.spark.mllib.stat.test.KolmogorovSmirnovTestResult
+
+object KolmogorovSmirnovOLS {
+
+  val sc = new SparkContext("local", "ks")
+
+  def main(args: Array[String]): Unit = {
+
+    val n = 1e2.toLong
+    val x1 = normalRDD(sc, n, seed = 10L)
+    val x2 = normalRDD(sc, n, seed = 5L)
+    val X = x1.zip(x2)
+    val y = X.map(x => 2.5 + 3.4 * x._1 + 1.5 * x._2)
+    val data = y.zip(X).map { case (yi, (xi1, xi2)) => (yi, xi1, xi2)}
+
+    val iters = 100
+    val goodModelData = data.map(goodSpec)
+    val goodModel = LinearRegressionWithSGD.train(goodModelData, iters)
+
+    val badModelData = data.map(badSpec)
+    val badModel = LinearRegressionWithSGD.train(badModelData, iters)
+
+    val goodResiduals = goodModelData.map(p => p.label - goodModel.predict(p.features))
+    val badResiduals = badModelData.map(p => p.label - badModel.predict(p.features))
+
+    println(testResiduals(goodResiduals))
+    println(testResiduals(badResiduals))
+  }
+
+  // This model specification captures all the regressors that underly
+  // the true process
+  def goodSpec(obs: (Double, Double, Double)): LabeledPoint = {
+    val X = Array(1.0, obs._2, obs._3)
+    val y = obs._1
+    LabeledPoint(y, Vectors.dense(X)) //
+  }
+
+  def badSpec(obs: (Double, Double, Double)): LabeledPoint = {
+    val X  = Array(1.0, obs._2, obs._2 * obs._1)
+    val y = obs._1
+    LabeledPoint(y, Vectors.dense(X))
+  }
+
+  // We'd like to compare our residuals against the standard normal distribution
+  // But we'd like to use the 2-sample Kolmogorov-Smirnov test, so we'll draw a sample
+  // from that distribution and compare
+  def testResiduals(residuals: RDD[Double]): KolmogorovSmirnovTestResult = {
+    val stdNormSample = normalRDD(sc, residuals.count())
+    val mean = residuals.mean()
+    val variance = residuals.map(x => (x - mean) * (x - mean)).mean()
+    val standardizedResiduals = residuals.map(x => (x - mean) / math.sqrt(variance))
+    // TODO: uncomment below once 2-sample included in MLlib
+    //Statistics.kolmogorovSmirnovTest2(residuals, stdNormSample)
+    Statistics.kolmogorovSmirnovTest(standardizedResiduals, "norm")
+  }
+}
